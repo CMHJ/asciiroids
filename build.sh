0a2cf77b3260657355438d4c861342dd1bbe6e07
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-08-19 21:15:57 +1000

    add debug symbols to output binary

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 TOP="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p "$TOP/build"
-gcc -std=c99 -o "$TOP"/build/asciiroids "$TOP"/src/main.c
+gcc -std=c99 -g -o "$TOP"/build/asciiroids "$TOP"/src/main.c

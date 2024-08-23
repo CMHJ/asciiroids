b914358227c7cfe77565fc6999fc3763d7cf0842
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-08-23 18:04:42 +1000

    updated build script to exit on error and set root bit on binary

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+set -e # exit on error
+
+# if not root rerun as root
+if [ "$(whoami)" != "root" ]; then
+    exec sudo "$0" "$@"
+fi
+
 TOP="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p "$TOP/build"
@@ -9,3 +16,6 @@ gcc \
     -Wall -Wextra -Wpedantic \
     -o "$TOP"/build/asciiroids \
     "$TOP"/src/main.c
+
+chown root:$(id -g) "$TOP"/build/asciiroids
+chmod u+s "$TOP"/build/asciiroids

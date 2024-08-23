371e618d0884a209ed30582386a3ff6b3f77f2c1
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-08-23 17:04:32 +1000

    added warnings to build script

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@
 TOP="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p "$TOP/build"
-gcc -std=c99 -g -o "$TOP"/build/asciiroids "$TOP"/src/main.c
+gcc \
+    -std=c99 \
+    -g \
+    -Wall -Wextra -Wpedantic \
+    -o "$TOP"/build/asciiroids \
+    "$TOP"/src/main.c

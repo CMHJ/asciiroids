f17720c701f0da3b0e74f86fd1c85479ea400a12
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-08-23 17:03:48 +1000

    added types header

---
+++ src/types.h
@@ -0,0 +1,21 @@
+#ifndef ASCIIROIDS_TYPES_H
+#define ASCIIROIDS_TYPES_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef size_t usize;
+typedef ptrdiff_t isize;
+
+#endif // ASCIIROIDS_TYPES_H

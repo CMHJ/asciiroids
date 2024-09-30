fc838bff1414fa68d3d5266da1a5f37d7ae66901
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-09-30 20:35:28 +1000

    removed duplicated constants

--- src/asciiroids.h
+++ src/asciiroids.h
@@ -7,16 +7,6 @@ const wchar_t no_shade = L' ';
 const wchar_t light_shade = L'\u2591';
 const wchar_t dark_shade = L'\u2593';
 
-// TODO(CMHJ): move these constants inside the array
-const wchar_t U_SHIP_N = L'\u25B2';
-const wchar_t U_SHIP_E = L'\u25BA';
-const wchar_t U_SHIP_S = L'\u25BC';
-const wchar_t U_SHIP_W = L'\u25C4';
-const wchar_t U_SHIP_SE = L'\u25E2';
-const wchar_t U_SHIP_SW = L'\u25E3';
-const wchar_t U_SHIP_NW = L'\u25E4';
-const wchar_t U_SHIP_NE = L'\u25E5';
-
 // describe segment which is the direction that the ship is pointing
 typedef enum ship_direction {
     EAST = 0,
@@ -30,9 +20,18 @@ typedef enum ship_direction {
     TOTAL_DIRECTIONS
 } ship_direction;
 
+const wchar_t U_SHIP_N = L'\u25B2';
+const wchar_t U_SHIP_E = L'\u25BA';
+const wchar_t U_SHIP_S = L'\u25BC';
+const wchar_t U_SHIP_W = L'\u25C4';
+const wchar_t U_SHIP_SE = L'\u25E2';
+const wchar_t U_SHIP_SW = L'\u25E3';
+const wchar_t U_SHIP_NW = L'\u25E4';
+const wchar_t U_SHIP_NE = L'\u25E5';
+
 // starts east at 0 degrees and goes counter clockwise
-const wchar_t SHIP_CHARS[TOTAL_DIRECTIONS] = {L'\u25BA', L'\u25E5', L'\u25B2', L'\u25E4',
-                                              L'\u25C4', L'\u25E3', L'\u25BC', L'\u25E2'};
+const wchar_t SHIP_CHARS[TOTAL_DIRECTIONS] = {U_SHIP_E, U_SHIP_NE, U_SHIP_N, U_SHIP_NW,
+                                              U_SHIP_W, U_SHIP_SW, U_SHIP_S, U_SHIP_SE};
 
 #define FPS 60
 

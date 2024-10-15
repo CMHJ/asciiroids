173d1326a3dcd2d6a804422ebd4815f8c61d03cd
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-10-15 19:37:33 +1100

    added degrees utility funcs

--- src/utility.c
+++ src/utility.c
@@ -4,6 +4,10 @@ static inline f32 to_radians(const f32 degrees) {
     return degrees * (PI / 180.0f);
 }
 
+static inline f32 to_degrees(const f32 radians) {
+    return radians * (180.0f / PI);
+}
+
 static inline f32 v2_mag(const v2 v) {
     return sqrtf((v.x * v.x) + (v.y * v.y));
 }
@@ -22,5 +26,19 @@ static f32 degrees_clip(f32 angle) {
         angle += DEG_360;
     }
 
+    return angle;
+}
+
+static f32 deg_sin(f32 x) {
+    return sinf(to_radians(x));
+}
+
+static f32 deg_cos(f32 x) {
+    return cosf(to_radians(x));
+}
+
+static f32 deg_atan2(f32 y, f32 x) {
+    f32 angle = atan2f(y, x);
+    angle = to_degrees(angle);
     return angle;
 }
--- src/utility.h
+++ src/utility.h
@@ -6,8 +6,12 @@
 #include "constants.h"
 
 static inline f32 to_radians(const f32 degrees);
+static inline f32 to_degrees(const f32 radians);
 static inline f32 v2_mag(const v2 v);
 static void buffer_clear(screen_buffer* buffer);
 static f32 degrees_clip(f32 angle);
+static f32 deg_sin(f32 x);
+static f32 deg_cos(f32 x);
+static f32 deg_atan2(f32 y, f32 x);
 
 #endif  // ASCIIROIDS_UTILITY_H

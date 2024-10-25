a2c94cf9aa448e91d0cc5c98e9f16aa269eb2084
CMHJ <55470933+CMHJ@users.noreply.github.com>
2024-10-25 15:12:11 +1100

    added rendering for saucers

--- src/enemy.c
+++ src/enemy.c
@@ -24,6 +24,16 @@ static void render_enemy_asteroid_large(screen_buffer* buffer, v2 pos) {
     print_xy(buffer, (usize)pos.x, (usize)pos.y + 1, layer, 4);
 }
 
+static void render_enemy_saucer_small(screen_buffer* buffer, v2 pos) {
+    static wchar_t layer[2] = {SAUCER_SMALL_LEFT, SAUCER_SMALL_RIGHT};
+    print_xy(buffer, (usize)pos.x, (usize)pos.y, layer, 2);
+}
+
+static void render_enemy_saucer_large(screen_buffer* buffer, v2 pos) {
+    static wchar_t layer[4] = {SAUCER_LARGE_LEFT, SAUCER_LARGE_LEFT_MID, SAUCER_LARGE_RIGHT_MID, SAUCER_LARGE_RIGHT};
+    print_xy(buffer, (usize)pos.x, (usize)pos.y, layer, 4);
+}
+
 static void render_enemies(screen_buffer* buffer, enemy_state* enemies) {
     for (u8 i = 0; i < MAX_ENEMIES; ++i) {
         switch (enemies[i].type) {
@@ -41,6 +51,14 @@ static void render_enemies(screen_buffer* buffer, enemy_state* enemies) {
             }
             case ASTEROID_LARGE: {
                 render_enemy_asteroid_large(buffer, enemies[i].phy.pos);
+                break;
+            }
+            case SAUCER_SMALL: {
+                render_enemy_saucer_small(buffer, enemies[i].phy.pos);
+                break;
+            }
+            case SAUCER_LARGE: {
+                render_enemy_saucer_large(buffer, enemies[i].phy.pos);
                 break;
             }
             default: {

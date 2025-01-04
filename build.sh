e58fc770f05a46b4c0f7f9c2e4469d5ff6f997c9
CMHJ <55470933+CMHJ@users.noreply.github.com>
2025-01-04 11:38:36 +1100

    changed build dirs to be separate

--- build.sh
+++ build.sh
@@ -5,21 +5,23 @@ set -e # exit on error
 TOP="$(dirname "$(readlink -f "$0")")"
 OPTIONS="-std=c99 -O0 -g -Wall -Wextra -Wpedantic -fsanitize=address,undefined"
 
-mkdir -p "$TOP/build"
-
 case "$1" in
 release)
-    gcc -std=c99 -DRELEASE -g -o "$TOP"/build/asciiroids \
+    mkdir -p "$TOP/build/release"
+    gcc -std=c99 -DRELEASE -g -o "$TOP"/build/release/asciiroids \
         "$TOP"/src/main.c \
         -lm
+    strip "$TOP"/build/release/asciiroids
     ;;
 *)
+    mkdir -p "$TOP/build/debug"
+
     # TODO(CMHJ): work out why asan is cracking the shits about global constants
-    gcc -fPIC -shared -std=c99 -g -o "$TOP"/build/libasciiroids.so "$TOP"/src/asciiroids.c
+    gcc -fPIC -shared -std=c99 -g -o "$TOP"/build/debug/libasciiroids.so "$TOP"/src/asciiroids.c
 
     gcc \
         $OPTIONS \
-        -o "$TOP"/build/asciiroids \
+        -o "$TOP"/build/debug/asciiroids \
         "$TOP"/src/main.c
     ;;
 esac
